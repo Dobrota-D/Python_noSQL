09ffe2678d1b0a3e0258a394424a26a5e410f987
Dobrota <dobrotakevin@yahoo.fr>
2021-10-21 23:05:22 +0200

    comment

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, request
 import pymongo
+from flask import Flask, request
 
+
+"""
+Her we gonna init the link between the mongodb database and the app.py, by using the admin account in the user_name
+password to access the database and collections and use flask for the root the local server.
+"""
 
 user_name = "martin"
 password = "billybob"
@@ -12,44 +17,44 @@ collection_directors = db.directors
 
 app = Flask(__name__)
 
+
 @app.route("/animes", methods=["GET"])
 def afficher_anime():
-
-
     """
         We are going to sort anime by alphebitical and acreasing order
     """
     yes = collection_animes.find().sort("title")
 
-    #for x in yes:
-        #print(x)
+    # for x in yes:
+    # print(x)
 
     """
         We are going to sort anime by decreasing order
     """
     yes = collection_animes.find().sort("title", -1)
 
-    #for x in yes:
-        #print(x)
+    # for x in yes:
+    # print(x)
 
     """
          We are going to sort directors by alphebitical and acreasing order
     """
     yes = collection_directors.find().sort("firstname")
 
-    #for x in yes:
-        #print(x)
+    # for x in yes:
+    # print(x)
 
     """
         We are going to sort directors by decreasing order
     """
-    #life = collection_directors.find().sort("firstname", -1)
+    # life = collection_directors.find().sort("firstname", -1)
     anime_list = list(collection_animes.find())
 
     return \
         {
             "anime_list": anime_list
         }
+
 
 @app.route("/animes", methods=["POST"])
 def create_anime():
@@ -60,11 +65,10 @@ def create_anime():
     new_anime = {
         '_id': 'e'
     }
-    #collection_animes.insert_one(new_anime)
+    # collection_animes.insert_one(new_anime)
 
-    return { 'code': 200, 'msg': 'Nouvel animé créé' }
+    return {'code': 200, 'msg': 'Nouvel animé créé'}
 
 
 if __name__ == "__main__":
     afficher_anime()
-
